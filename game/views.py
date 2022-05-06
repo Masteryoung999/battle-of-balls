ea7acbf988cdff794fa07047e2dc8ea48bdc7e08
Master Young <834925473@qq.com>
2022-05-06 08:42:08 +0800

    add pictures

--- game/views.py
+++ game/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align: center">Master Young</h1>'
+    line2 = '<img src="https://pic.rmb.bdstatic.com/1530971282b420d77bdfb6444d854f952fe31f0d1e.jpeg" width=1500>'
+    line3 = '<hr>'
+    return HttpResponse(line1 + line3 + line2)
+def play(request):
+    line1 = '<h1 style = "text-align: center">游戏界面<h1>'
+
+    line2 = '<img src="https://pic.rmb.bdstatic.com/1530971282b420d77bdfb6444d854f952fe31f0d1e.jpeg" width=1500>'
+    return HttpResponse(line1 + line2)
