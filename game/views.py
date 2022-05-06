122f8421d44fda822039e985b8fd72e18c91e62d
Master Young <834925473@qq.com>
2022-05-06 09:08:55 +0800

    modify picture

--- game/views.py
+++ game/views.py
@@ -8,7 +8,7 @@ def index(request):
     line4 = '<a href = "/play/">进入游戏界面</a>'
     return HttpResponse(line1 + line4 + line3 + line2)
 def play(request):
-    line1 = '<h1 style = "text-align: center">游戏界面<h1>'
+    line1 = '<h1 style = "text-align: center">紫霞仙子<h1>'
 
     line2 = '<img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwallpaperm.cmcm.com%2F04d87401629cc049d3ef0f5700ceca8a.jpg&refer=http%3A%2F%2Fwallpaperm.cmcm.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654390142&t=075c36f008208a45c2708f37c45860a0" width=1500>'
 
