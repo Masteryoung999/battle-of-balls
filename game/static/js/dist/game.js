a1f3a170910e57d9f8fe61fc535ff6d799d593d2
Master Young <834925473@qq.com>
2022-05-11 00:00:05 +0800

    set three bottoms

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -3,9 +3,23 @@ class AcGameMenu{
         this.root = root;
         this.$menu = $(`
 <div class="ac-game-menu">
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-field-item-single">
+            单人模式
+        </div>
+        <div class="ac-game-menu-field-item ac-game-field-item-multi">
+            多人模式
+        </div>
+        <div class="ac-game-menu-field-item ac-game-field-item-settings">
+            设置
+        </div>
+    </div>
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
+        this.$single = this.$menu.find('.ac-game-field-item-single');
+        this.$multi = this.$menu.find('.ac-game-field-item-multi');
+        this.$settings = this.$menu.find('.ac-game-field-item-settings');
     }
 }
 class AcGame{
