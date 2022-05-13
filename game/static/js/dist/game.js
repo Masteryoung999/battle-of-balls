b968bd1c5745188bbdd424d3ac0d243e9fc6fbbe
Master Young <834925473@qq.com>
2022-05-13 12:18:47 +0800

    write canvas

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -51,14 +51,88 @@ class AcGameMenu {
     hide() {  // 关闭menu界面
         this.$menu.hide();
     }
+}let AC_GAME_OBJECTS = [];
+
+class AcGameObject {
+    constructor() { //构造函数
+        AC_GAME_OBJECTS.push(this);
+
+        this.has_called_start = false;  // 判断是否执行过start函数
+        this.timedelta = 0;  //当前帧距离上一帧的时间间隔
+    }
+
+    start() { //只会在第一帧执行一次
+
+    }
+
+    update() { //每一帧都会执行一次
+
+    }
+
+    on_destroy() { //在被销毁前执行一次
+        
+    }
+
+    destroy() { //删掉该物体
+        this.on_destroy();
+        for(let i = 0; i < AC_GAME_OBJECTS.length; i ++) {
+            if(AC_GAME.OBJECTS[i] === this) {
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+let last_timestamp;
+
+let AC_GAME_ANIMATION = function(timestamp) {
+    for(let i = 0; i < AC_GAME_OBJECTS.length; i ++){
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start) {
+            obj.start();
+            obj.has_called_start = true;
+        } else {
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);class GameMap extends AcGameObject {
+    constructor(playground) {
+        super();
+        this.playground = playground;
+        this.$canvas = $(`<canvas></canvas>`);
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.playground.$playground.append(this.$canvas);
+    }
+
+    start() {
+        
+    }
+    update() {
+        this.render();
+    }
+    render() {
+        this.ctx.fillStyle = "rgba(0, 0, 0)";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
 }class AcGamePlayground{
     constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
 
-        this.hide();
+        // this.hide();
         this.root.$ac_game.append(this.$playground);
-
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
         this.start();
     }
 
@@ -72,11 +146,11 @@ class AcGameMenu {
     hide(){  //关闭playground界面
         this.$playground.hide();
     }
-}class AcGame{
+}export class AcGame{
     constructor(id){
         this.id = id;
         this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
+        //this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
         this.start();
     }
